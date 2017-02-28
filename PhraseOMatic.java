666dcafdc3de85646a7eb15e13e2f8c88ade9090
rylan <rylangooch@gmail.com>
2017-02-28 11:31:31 +0000

    Added Phrase-O-Matic program

---
+++ PhraseOMatic.java
@@ -0,0 +1,27 @@
+public class PhraseOMatic {
+  public static void main (String[] args) {
+
+    String[] wordListOne = {"24/7", "Charlie", "Spatula", "Smart",
+    "Front-end"};
+
+    String[] wordListTwo = {"Empowered", "Aligned", "Leveraged", "Focused",
+    "Peachy"};
+
+    String[] wordListThree = {"Political", "Economical", "Philosophical", "Wary",
+    "Updated"};
+
+    int oneLength = wordListOne.length;
+    int twoLength = wordListTwo.length;
+    int threeLength = wordListThree.length;
+
+    int rand1 = (int) (Math.random() * oneLength);
+    int rand2 = (int) (Math.random() * twoLength);
+    int rand3 = (int) (Math.random() * threeLength);
+
+    String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " +
+    wordListThree[rand3];
+
+    System.out.println("What we need is a " + phrase);
+
+  }
+}
