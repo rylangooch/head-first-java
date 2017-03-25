6e67f1f88c88a2c3baca5b4e82821d143eecaf38
rylan <rylangooch@gmail.com>
2017-03-25 14:33:24 +0000

    Added complete Dot Com Bust game

--- c5/DotCom.java
+++ c5/DotCom.java
@@ -2,26 +2,28 @@ import java.util.ArrayList;
 
 public class DotCom {
 
-  private ArrayList<String> location cells;
-  // private int numOfHits;
+  private ArrayList<String> locationCells;
+  private String name;
 
   public void setLocationCells(ArrayList<String> locs) {
     locationCells = locs;
   }
 
+  public void setName(String n){
+    name = n;
+  }
+
   public String checkYourself(String userInput) {
     String result = "miss";
-
     int index = locationCells.indexOf(userInput);
-
     if(index >= 0) {
-
       locationCells.remove(index);
 
       if (locationCells.isEmpty()) {
         result = "kill";
+        System.out.println("Ouch! You sunk " + name + " :(");
       } else {
-        result = "hit"
+        result = "hit";
       }
     }
     return result;
